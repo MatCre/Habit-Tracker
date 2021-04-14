1ef63172d82f76fd4a729fe6a05b702321dbd74c
Mat Donley <73589410+MatCre@users.noreply.github.com>
2021-04-14 09:47:19 +0100

    outer shell of app, card css, habit form created

---
+++ habit-tracker/src/components/StyledComponentsExample.js
@@ -0,0 +1,36 @@
+// export const theme = {
+//     niceYellow: '#F1E100',
+//     font: "sans-serif",
+//     niceBlue: '#1919E6'
+// };
+
+
+
+
+
+// const Card = styled.div`
+//     max-width: 350px;
+//     border: 5px solid ${props => props.theme.niceYellow};
+//     border-radius: 5px;
+//     overflow: hidden;
+//     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+//     margin: 30px auto;
+//     button {
+//         display: block;
+//         width: 100%;
+//     }
+// `
+
+// const HabitCardContainer = () => (
+//     <Card>
+//         <HabitCard />
+//     </Card>
+// );
+
+
+// const GlobalStyles = styled.div`
+//     font-family: ${props => props.theme.font};
+//     color: ${props => props.theme.niceBlue};
+//     display: flex;
+//     justify-content: center;
+// `
